16f4e63ff5c57d745a12ab14af9924693649e112
WallochMatt <matthewrwalloch@gmail.com>
2022-09-23 14:36:11 -0500

    feat: Added status codes to the function returns in views

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def products_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerialiazer(products, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
         serializer = ProductSerialiazer(data=request.data)
@@ -25,12 +25,14 @@ def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerialiazer(product)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
     elif request.method == "PUT":
         serializer = ProductSerialiazer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
