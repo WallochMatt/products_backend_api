59c490ac98e6b357113b4c77e1d4defe6cdb51bb
WallochMatt <matthewrwalloch@gmail.com>
2022-09-23 13:21:14 -0500

    feat: Created new view function for GET product by id + Added new URLs path to products_detail view function

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
+from itertools import product
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerialiazer
 from .models import Product
+from products import serializers
 
 @api_view(['GET', 'POST'])
 def products_list(request):
@@ -17,6 +19,14 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerialiazer(product)
+        return Response(serializer.data)
+    
 
 
 
