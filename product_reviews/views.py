8694fa8cc571a211a74f7aad40e4703ba03864de
WallochMatt <matthewrwalloch@gmail.com>
2022-09-23 16:56:14 -0500

    feat: Completed GET and POST function for standard endpoint + Seeded product reviews into database

--- product_reviews/views.py
+++ product_reviews/views.py
@@ -1,8 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializer import ReviewSerializer
+from .models import Review
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def reviews_list(request):
-    
+    if request.method == 'GET':
+        products = Review.objects.all()
+        serializer = ReviewSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    return Response('ok')
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
