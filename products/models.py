a0f19f270cf2ae5216e3c91f0c89bbf46fdcf73d
WallochMatt <matthewrwalloch@gmail.com>
2022-09-26 16:12:59 -0500

    fix: Updated product model image link to have a default value

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
-    image_link = models.CharField(max_length=255)
+    image_link = models.CharField(max_length=255, default='https://as2.ftcdn.net/v2/jpg/00/89/55/15/1000_F_89551596_LdHAZRwz3i4EM4J0NHNHy2hEUYDfXc0j.jpg')
  
